ed34396a6ad47e3c0c935c3275f382875ddb62ec
grega913 <grega913@gmail.com>
2017-12-20 10:48:00 +0100

    Add new messagge and create message events

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+var socket = io(); //initiating a request from the client and 
+//keep the connection open - > and store it in a variable
+
+socket.on('connect', function() {
+    console.log('Connected to server!')
+
+    /*
+    socket.emit('createEmail', {
+        to:'jan@example.com',
+        text:'Hey, this is andrew'
+    })
+    */
+
+    socket.emit('createMessage', {
+        from:'Janko the client',
+        text:'I am just sending this to say hello!!'
+    })
+    
+})
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server!')
+})
+
+
+//listen to custom event
+/*
+socket.on('newEmail', function(email) {
+    console.log("new email", email)
+})
+*/
+
+socket.on('newMessage', function(message) {
+    console.log("new message", message)
+})
--- server/server.js
+++ server/server.js
@@ -17,6 +17,31 @@ app.use(express.static(publicPath))
 io.on('connection', (socket)=> {
     console.log("new user connected")
 
+    /*
+    socket.emit('newEmail', {
+        from:'mike@example.com',
+        text:'hey, what is going on',
+        createdAt:123
+    })
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail)
+    })
+    */
+
+    socket.emit('newMessage', {
+        from:"admin guy",
+        text:"welcome",
+        createdAt: new Date()
+    })
+
+
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message)
+    })
+
+
     socket.on('disconnect',()=>{
         console.log('User was disconnected!')
     })

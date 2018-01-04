3dde6442330d3fdad3edf3daa550bb78ad452b25
grega913 <grega913@gmail.com>
2018-01-04 14:35:00 +0100

    css added

--- public/js/index.js
+++ public/js/index.js
@@ -3,34 +3,11 @@ var socket = io(); //initiating a request from the client and
 
 socket.on('connect', function() {
     console.log('Connected to server!')
-
-    /*
-    socket.emit('createEmail', {
-        to:'jan@example.com',
-        text:'Hey, this is andrew'
-    })
-    */
-
-    /*
-    socket.emit('createMessage', {
-        from:'Janko the client',
-        text:'I am just sending this to say hello!!'
-    })
-    */
-    
 })
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server!')
 })
-
-
-//listen to custom event
-/*
-socket.on('newEmail', function(email) {
-    console.log("new email", email)
-})
-*/
 
 socket.on('newMessage', function(message) {
     console.log("new message", message)
@@ -49,17 +26,18 @@ socket.on('newLocationMessage', function (message) {
     li.append(a)
     jQuery('#messages').append(li)
 
-
 })
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault()
 
+    var messageTextbox = jQuery('[name=message]')
+
     socket.emit('createMessage', {
         from:'User',
-        text:jQuery('[name=message]').val()
+        text:messageTextbox.val()
     }, function() {
-
+        messageTextbox.val('')
     })
 })
 
@@ -69,12 +47,16 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported by your browser!')
     }
 
+    locationButton.attr('disabled','disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location')
         socket.emit('createLocationMessage', {
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
         })
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location')
         alert('Unable to fetch location.')
     })
 
